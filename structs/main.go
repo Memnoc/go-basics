1efc50cc909cafd921cd493bf532286342eedc90
Memnoc <smartdroidesign@gmail.com>
2024-04-02 05:50:15 +0100

    embedded structs

--- structs/main.go
+++ structs/main.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
+type ContactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
-	firstName string
-	lastName  string
+	firstName   string
+	lastName    string
+	contactInfo ContactInfo
 }
 
 func main() {
 	fmt.Println("== Structs ==")
 
-	jim := person{"Jim", "Halpert"}
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contactInfo: ContactInfo{
+			email:   "jhalper@dundermifflin.com",
+			zipCode: 94000,
+		},
+	}
 
 	fmt.Println("My first name is: ", jim.firstName)
 	fmt.Println("My last name is: ", jim.lastName)
+	fmt.Printf("Person info: %+v\n", jim)
 }
