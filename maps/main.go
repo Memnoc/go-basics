6534d3957eed8793d2f8efee14617a318a92105f
Memnoc <smartdroidesign@gmail.com>
2024-04-03 06:19:34 +0100

    maps iterating, adding, deleting

--- maps/main.go
+++ maps/main.go
@@ -5,10 +5,25 @@ import "fmt"
 func main() {
 	fmt.Println("== Maps ==")
 
-	colors := map[string]string{
+	colors := map[string]string{ // declaring
 		"red":   "#ff000",
-		"green": "4b475",
+		"green": "#4b475",
 	}
 
-	fmt.Println(colors)
+	colors["white"] = "#fffff" // adding
+
+	fmt.Println("Accessing: ", colors["white"]) // accessing
+
+	delete(colors, "white") // deleting key
+
+	for _, color := range colors {
+		fmt.Println("Iterating: ", color)
+	}
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("printMap(): Hex code for color ", color, "is", hex)
+	}
 }
